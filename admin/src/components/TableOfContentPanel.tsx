be62323fcb431a9c11f10fae71a0f749b24cceaf
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2025-10-02 17:18:30 +0200

    feat: ToC

--- admin/src/components/TableOfContentPanel.tsx
+++ admin/src/components/TableOfContentPanel.tsx
@@ -1,17 +1,25 @@
 import type { PanelComponent } from '@strapi/content-manager/strapi-admin'
 
-import { unstable_useContentManagerContext as useContentManagerContext } from '@strapi/content-manager/strapi-admin'
+import { unstable_useContentManagerContext as useContentManagerContext } from '@strapi/strapi/admin'
+import { Typography } from '@strapi/design-system'
 
 const TableOfContentPanel: PanelComponent = (_props) => {
-
   const { form } = useContentManagerContext()
 
   return {
     title: 'Table of Content',
     content: (
-      <div>
-        {JSON.stringify(form)}
-      </div>
+      <ol style={{ width: '100%'}}>
+        { Object.entries((form as any).values).map(([key, value]) => {
+          return (
+            <li key={key}>
+              <Typography>
+                {key}: {(value as any).toString()}
+              </Typography>
+            </li>
+          )
+        }) }
+      </ol>
     ),
   }
 }
