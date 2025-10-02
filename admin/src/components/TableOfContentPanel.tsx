60079f4add04222f8975df6a63ea50ec7ece4114
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2025-10-02 19:10:09 +0200

    feat: Table of Content POC

--- admin/src/components/TableOfContentPanel.tsx
+++ admin/src/components/TableOfContentPanel.tsx
@@ -1,10 +1,14 @@
 import type { PanelComponent } from '@strapi/content-manager/strapi-admin'
 
-import { unstable_useContentManagerContext as useContentManagerContext } from '@strapi/strapi/admin'
-import { Typography } from '@strapi/design-system'
+import { unstable_useContentManagerContext as useContentManagerContext, unstable_useDocumentLayout as useDocumentLayout } from '@strapi/strapi/admin'
+import { Button, Typography } from '@strapi/design-system'
 
-const TableOfContentPanel: PanelComponent = (_props) => {
+const TableOfContentPanel: PanelComponent = (props) => {
   const { form } = useContentManagerContext()
+  const { edit } = useDocumentLayout(props.model)
+
+  console.log((form as any).values)
+  console.log(edit)
 
   return {
     title: 'Table of Content',
@@ -13,9 +17,15 @@ const TableOfContentPanel: PanelComponent = (_props) => {
         { Object.entries((form as any).values).map(([key, value]) => {
           return (
             <li key={key}>
-              <Typography>
-                {key}: {(value as any).toString()}
-              </Typography>
+              { props.activeTab === 'published' ? (
+                <Typography>
+                  {key}: {(value as any).toString()}
+                </Typography>
+              ) : (
+                <Button size="S" variant="ghost">
+                  {key}: {(value as any).toString()}
+                </Button>
+              )}
             </li>
           )
         }) }
